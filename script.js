ed074b1ff43eabcb6d1026f3889d0691c1752aed
HusseinRmaile <hhrmaile@gmail.com>
2025-02-19 11:45:56 -0500

    fixed elemnt name

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById("groupNum").addEventListener("click", function() {
+document.getElementById("members").addEventListener("click", function() {
     alert("Hussein Rmaile, Aarthi Kannan, Suhana Shirol, Trisha Nittala, Divya Sharma");
 });
