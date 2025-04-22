a4600e4b54f9e67a2b692a783b2dd258d435da5c
HusseinRmaile <hhrmaile@gmail.com>
2025-04-22 16:58:25 -0400

    added new html file for final report, updated navbar to include its respective button as well

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const projectButton = document.getElementById("btn-proposal");
+    const proposalButton = document.getElementById("btn-proposal");
     const midtermButton = document.getElementById("btn-midterm");
+    const finalButton = document.getElementById("btn-midterm");
 
-    projectButton.addEventListener("click", () => {
-        window.location.href = "index.html";  // Adjust if needed
+    proposalButton.addEventListener("click", () => {
+        window.location.href = "index.html";  // proposal button
     });
 
     midtermButton.addEventListener("click", () => {
-        window.location.href = "midterm.html";  // Replace with actual file path
+        window.location.href = "midterm.html";  // midterm checkpoint button
+    });
+
+    finalButton.addEventListener("click", () => {
+        window.location.href = "final.html"; // final checkpoint button
     });
 });
